314c8f13adc14ba0c2a1ac15b940940c87fb1f68
Seraph08 <davidvandenburgh@linux.com>
2017-02-11 11:30:13 -0500

    adapt to be self policing

--- cmhw/org/cyanogenmod/hardware/UniqueDeviceId.java
+++ cmhw/org/cyanogenmod/hardware/UniqueDeviceId.java
@@ -18,7 +18,7 @@ package org.cyanogenmod.hardware;
 
 import android.util.Log;
 
-import org.cyanogenmod.internal.util.FileUtils;
+import org.cyanogenmod.hardware.util.FileUtils;
 
 /**
  * Generate a unique but deterministic ID for this hardware, based on unchangeable
